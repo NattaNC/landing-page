884aeb8841a0c7c3ea20973d61da948fc2591623
nattanc <naathancaue556@gmail.com>
2022-12-19 20:21:23 -0300

    Finalização projeto

---
+++ landing-page-url-shortly/assets/JavaScript/main.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector('.menu-mobile')
+const pop = document.querySelector('.PopUp')
+
+menu.addEventListener('click', Abrir)
+function Abrir() {
+    pop.style.display = 'flex'
+}
+

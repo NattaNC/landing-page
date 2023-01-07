05082740b182ef1c1aee24104edcc5a752098e01
nattanc <naathancaue556@gmail.com>
2023-01-07 18:38:44 -0300

    Landing Page Finalizada

--- landing-page-url-shortly/assets/JavaScript/main.js
+++ landing-page-url-shortly/assets/JavaScript/main.js
@@ -3,27 +3,75 @@ $('#hamburger').on('click', function() {
     $('#PopUp').slideToggle('slow');
   });
   
-  $('.menu-link').each(function() {
-    $(this).on('click', function() {
-      $('#menu').slideToggle('slow');
-    });
-  });
+  
+
 
   /*Mensagem error */
-  const InputEcurtador = document.getElementById('EncurtarLink')
-  const Shorten = document.getElementById('btn-input')
   const card = document.getElementById('card-generate')
 
-  Shorten.addEventListener('click', EncurtarLink)
+  document.getElementById('btn-input').addEventListener('click', EncurtarLink)
   function EncurtarLink() {
-    InputEcurtadorTeste = InputEcurtador.value
-    if (InputEcurtadorTeste === '') {
-        document.getElementById('EncurtarLink').style.border = 'solid 1px red'
-        document.getElementById('invalid').innerHTML = `<em>Please add a link</em>`
+    let url = document.getElementById('EncurtarLink').value;
 
-    } else {
-        document.getElementById('card-generate').style.display = 'block'
+    //api key: fddb50cf2f8f4835889323c567cae747
+    
+    //encurtar o link
+
+    //headers
+    let headers = {
+      "Content-Type" : "application/json",
+      "apiKey" : "fddb50cf2f8f4835889323c567cae747"
     }
+
+    //dados
+    let linkRequest = {
+      destination: url,
+      domain: { fullName: "rebrand.ly"}
+    }
+
+    fetch('https://api.rebrandly.com/v1/links', {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(linkRequest)
+    }).then(Response => Response.json())
+      .then(json => {
+        let inputUrl = document.getElementById('EncurtarLink')
+        inputUrl.value = json.shortUrl
+        
+        if (!url) {
+          document.getElementById('EncurtarLink').style.border = 'solid 1px #ee1d1d'
+          document.getElementById('EncurtarLink').style.webkitTextFillColor = '#ee1d1d'
+          document.getElementById('invalid').innerHTML = `<em>Please add a link</em>`
+      } else {
+          document.getElementById('display').style.display = 'block'
+          document.getElementById('link').innerText = url
+          document.getElementById('link-encurtado').innerText = inputUrl.value
+        }
+      });
   }
+
+
+  //copiar url
+
+  document.getElementById('copied').addEventListener('click', copied)
+  function copied() {  
+    document.getElementById('copied').innerText = 'Copied!'
+
+    let inputUrl = document.getElementById('EncurtarLink')
+    
+    inputUrl.select()
+    inputUrl.setSelectionRange(0, 99999)
+
+    navigator.clipboard.writeText(inputUrl.value)
+  }
+
+
+
+
+
+
+
+  
+
 
 

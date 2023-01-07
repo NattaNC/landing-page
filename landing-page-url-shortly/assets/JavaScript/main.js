0654e2661f01ef5a164739a8aa6f2bbeaaeb8237
nattanc <naathancaue556@gmail.com>
2023-01-06 22:04:27 -0300

    Finalização projeto

--- landing-page-url-shortly/assets/JavaScript/main.js
+++ landing-page-url-shortly/assets/JavaScript/main.js
@@ -1,8 +1,29 @@
-const menu = document.querySelector('.menu-mobile')
-const pop = document.querySelector('.PopUp')
+/*Slide PopUp */
+$('#hamburger').on('click', function() {
+    $('#PopUp').slideToggle('slow');
+  });
+  
+  $('.menu-link').each(function() {
+    $(this).on('click', function() {
+      $('#menu').slideToggle('slow');
+    });
+  });
 
-menu.addEventListener('click', Abrir)
-function Abrir() {
-    pop.style.display = 'flex'
-}
+  /*Mensagem error */
+  const InputEcurtador = document.getElementById('EncurtarLink')
+  const Shorten = document.getElementById('btn-input')
+  const card = document.getElementById('card-generate')
+
+  Shorten.addEventListener('click', EncurtarLink)
+  function EncurtarLink() {
+    InputEcurtadorTeste = InputEcurtador.value
+    if (InputEcurtadorTeste === '') {
+        document.getElementById('EncurtarLink').style.border = 'solid 1px red'
+        document.getElementById('invalid').innerHTML = `<em>Please add a link</em>`
+
+    } else {
+        document.getElementById('card-generate').style.display = 'block'
+    }
+  }
+
 
